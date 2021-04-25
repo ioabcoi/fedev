86ffa46c024dac3602edf1667d4992a2c2992ee5
ioabcoi <ioabcoi@naver.com>
2021-04-25 18:49:03 +0900

    #12 javascriptForBeginners

--- javascriptForBeginners/background.js
+++ javascriptForBeginners/background.js
@@ -1,3 +1,5 @@
+/* image 
+
 const body = document.querySelector("body");
 
 const IMG_NUMBER = 3;
@@ -17,6 +19,32 @@ function getRandom(){
 function init(){
     const randomNumber = getRandom();
     paintImage(randomNumber);
+}
+
+init();
+*/
+
+/* background color */
+const body = document.querySelector("body");
+
+const COLOR_ARRAY = ["#000000", "#413c69", "#4a47a3", "#290149", "#4a3933", "#28527a", "#290149"];
+const COLOR_NUMBER = COLOR_ARRAY.length;
+
+function paintColor(colorNumber){
+    const div = document.createElement("div");
+    div.style.backgroundColor = COLOR_ARRAY[colorNumber];
+    div.classList.add("bgColor");
+    body.prepend(div);
+}
+
+function getRandom(){
+    const number = Math.floor(Math.random() * COLOR_NUMBER);
+    return number;
+}
+
+function init(){
+    const randomNumber = getRandom();
+    paintColor(randomNumber);
 }
 
 init();
