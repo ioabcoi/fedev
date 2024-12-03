82e26661f407d52ac38f0b31dae7c0bd8652b71a
구혜지 <ioabcoi@etoos.com>
2024-12-03 17:13:13 +0900

    2024-12-03

--- javascriptForBeginners/momentum/js/background.js
+++ javascriptForBeginners/momentum/js/background.js
@@ -7,10 +7,12 @@ const images = [
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 // console.log(chosenImage);
 
+const bg = document.querySelector("#bg");
 const bgImage = document.createElement("img");
 bgImage.src = `../chrome-app/images/${chosenImage}`;
 // console.log(bgImage);
-document.body.prepend(bgImage);
+// bg.prepend(bgImage);
+bg.style.backgroundImage = `url(${bgImage.src})`;
 // document.body.appendChild(bgImage);
 
 // 'https://picsum.photos/1920/1024/?random=' + num
